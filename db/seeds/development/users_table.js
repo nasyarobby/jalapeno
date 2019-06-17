d66881b3f269e1ffde39d719775b161ec684c6fb
Nasyarobby <nasyarobby@gmail.com>
2019-06-17 16:01:08 -0400

    adding hash function to seed file

--- db/seeds/development/users_table.js
+++ db/seeds/development/users_table.js
@@ -1,3 +1,5 @@
+let hash = require("./../../../libs/hash").hash
+
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
@@ -6,8 +8,8 @@ exports.seed = function (knex, Promise) {
       return knex('users').insert([{
           id: 1,
           email: 'alice@gmail.com',
-          username: 'alice',
-          password: 'secret',
+          username: 'alice001',
+          password: hash('secret'),
           name: 'Alice Peace',
           verification_code: null,
           verification_code_expired_at: null,
@@ -16,8 +18,8 @@ exports.seed = function (knex, Promise) {
         {
           id: 2,
           email: 'bob@gmail.com',
-          username: 'bobby',
-          password: 'secret',
+          username: 'bobby002',
+          password: hash('secret'),
           name: 'Bobby Bobo',
           verification_code: null,
           verification_code_expired_at: null,
@@ -26,8 +28,8 @@ exports.seed = function (knex, Promise) {
         {
           id: 3,
           email: 'charlie@gmail.com',
-          username: 'charlie',
-          password: 'secret',
+          username: 'charlie003',
+          password: hash('secret'),
           name: 'Charlie Charlotte',
           verification_code: null,
           verification_code_expired_at: null,
--- db/seeds/testing/users_table.js
+++ db/seeds/testing/users_table.js
@@ -1,3 +1,5 @@
+let hash = require("./../../../libs/hash").hash
+
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
@@ -7,7 +9,7 @@ exports.seed = function (knex, Promise) {
           id: 1,
           email: 'alice@gmail.com',
           username: 'alice001',
-          password: 'secret',
+          password: hash('secret'),
           name: 'Alice Peace',
           verification_code: null,
           verification_code_expired_at: null,
@@ -17,7 +19,7 @@ exports.seed = function (knex, Promise) {
           id: 2,
           email: 'bob@gmail.com',
           username: 'bobby002',
-          password: 'secret',
+          password: hash('secret'),
           name: 'Bobby Bobo',
           verification_code: null,
           verification_code_expired_at: null,
@@ -27,7 +29,7 @@ exports.seed = function (knex, Promise) {
           id: 3,
           email: 'charlie@gmail.com',
           username: 'charlie003',
-          password: 'secret',
+          password: hash('secret'),
           name: 'Charlie Charlotte',
           verification_code: null,
           verification_code_expired_at: null,
