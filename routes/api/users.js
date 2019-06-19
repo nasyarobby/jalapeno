100b185374a099ec4821d7c7dccb78147ae4f529
Nasyarobby <nasyarobby@gmail.com>
2019-06-18 20:51:48 -0400

    added /users/verify endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -69,6 +69,43 @@ router.put('/users/register', (req, res) => {
         })
 })
 
+router.post("/users/verify", (req, res) => {
+    let email = req.body.email;
+    let code = req.body.code;
+
+    User
+        .query()
+        .whereRaw("verification_code = ? AND email = ? AND verification_code_expired_at > NOW() ", [code, email])
+        .then(users => {
+            if (users.length > 0) {
+                return User
+                    .query()
+                    .where({
+                        id: users[0].id
+                    })
+                    .patchAndFetchById(users[0].id, {
+                        verification_code: null,
+                        verification_code_expired_at: null,
+                        verified_at: new Date()
+                    })
+            } else {
+                return false;
+            }
+        })
+        .then(user => {
+            res.setHeader('Content-Type', 'application/json');
+            if (user) {
+                delete user.password;
+                res.send(JSend.setSuccess(user).send());
+            } else
+                res.send(JSend.setFail({
+                    verification: [{
+                        message: "Invalid/expired verification code."
+                    }]
+                }).send());
+        })
+})
+
 router.post("/users/login", (req, res) => {
     let errors = [];
     if (!req.body || !req.body.login || !req.body.password) {
