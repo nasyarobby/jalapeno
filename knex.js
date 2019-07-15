6b59ead30fcf1d19abee835f2daacb93d231aabb
Nasyarobby <nasyarobby@gmail.com>
2019-07-15 13:15:42 -0400

    fixed: .env file is not loaded. npm run start now shows the environment value.

--- knex.js
+++ knex.js
@@ -1,4 +1,5 @@
+require("dotenv").config()
 var environment = process.env.NODE_ENV || 'development'
+console.log("Using environment:", environment)
 var config = require('./knexfile.js')[environment]
-
 module.exports = require('knex')(config)
