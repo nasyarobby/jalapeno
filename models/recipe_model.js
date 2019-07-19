ce6e00ed1c3007f9130b4231758052b2056b4e09
Nasyarobby <nasyarobby@gmail.com>
2019-07-19 12:13:20 -0400

    added cookbook relation mapping

--- models/recipe_model.js
+++ models/recipe_model.js
@@ -7,6 +7,24 @@ class Recipe extends Model {
     static get tableName() {
         return 'recipes';
     }
+    static get relationMappings() {
+        const Cookbook = require("./cookbook_model")
+
+        return {
+            cookbook: {
+                relation: Model.HasManyRelation,
+                modelClass: Cookbook,
+                join: {
+                    from: 'recipes.id',
+                    through: {
+                        from: 'cookbookrecipe.recipe_id',
+                        to: 'cookbookrecipe.cookbook_id'
+                    },
+                    to: 'cookbook.id'
+                }
+            }
+        }
+    }
 }
 
 module.exports = Recipe;
