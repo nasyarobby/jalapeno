142569c5f6d5686ba7784694bacfc0c99735870f
Nasyarobby <nasyarobby@gmail.com>
2019-07-23 18:28:04 -0400

    added recipes m2m relation mappings

--- models/category_model.js
+++ models/category_model.js
@@ -7,7 +7,24 @@ class Category extends Model {
     static get tableName() {
         return 'category';
     }
+    static get relationMappings() {
+        const Recipe = require("./recipe_model")
 
+        return {
+            recipes: {
+                relation: Model.ManyToManyRelation,
+                modelClass: Recipe,
+                join: {
+                    from: 'category.id',
+                    through: {
+                        from: 'recipecategory.category_id',
+                        to: 'recipecategory.recipe_id'
+                    },
+                    to: 'recipes.id'
+                }
+            }
+        }
+    }
 }
 
 module.exports = Category;
