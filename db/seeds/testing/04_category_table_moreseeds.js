427db2315433246cb881d4956ff5c16b80577b44
Nasyarobby <nasyarobby@gmail.com>
2019-07-23 00:23:28 -0400

    missing closing bracket.

--- db/seeds/testing/04_category_table_moreseeds.js
+++ db/seeds/testing/04_category_table_moreseeds.js
@@ -3,43 +3,44 @@
 exports.seed = function (knex, Promise) {
     // Deletes ALL existing entries
     return knex('category').del()
-      .then(function () {
-          // Inserts seed entries
-          return knex('category').insert([{
-              id: 1,
-              category: "Salad"
-          },
-          {
-              id: 2,
-              category: "Drink"
-          },
-          {
-              id: 3,
-              category: "Breakfast"
-          },
-          {
-              id: 4,
-              category: "Desserts"
-          },
-          {
-              id: 5,
-              category: "Italian"
-          },
-          {
-              id: 6,
-              category: "Sandwich"
-          },
-          {
-              id: 7,
-              category: "Soup"
-          },
-          {
-              id: 8,
-              category: "Bread"
-          },
-          {
-              id: 9,
-              category: "Appetizer"
-          }
-      ]);
-  });
+        .then(function () {
+            // Inserts seed entries
+            return knex('category').insert([{
+                    id: 1,
+                    category: "Salad"
+                },
+                {
+                    id: 2,
+                    category: "Drink"
+                },
+                {
+                    id: 3,
+                    category: "Breakfast"
+                },
+                {
+                    id: 4,
+                    category: "Desserts"
+                },
+                {
+                    id: 5,
+                    category: "Italian"
+                },
+                {
+                    id: 6,
+                    category: "Sandwich"
+                },
+                {
+                    id: 7,
+                    category: "Soup"
+                },
+                {
+                    id: 8,
+                    category: "Bread"
+                },
+                {
+                    id: 9,
+                    category: "Appetizer"
+                }
+            ]);
+        })
+}
