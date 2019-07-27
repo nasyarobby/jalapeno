608c3226351c517f3210940a4733d86e43e29a03
ElJalapenoVengador <eljalapenovengador@gmail.com>
2019-07-27 18:38:04 -0400

    Relation b/w ingredient and recipe models

--- models/ingredient_model.js
+++ models/ingredient_model.js
@@ -7,6 +7,24 @@ class Ingredients extends Model {
     static get tableName() {
         return 'ingredients';
     }
+    static get relationMappings() {
+        const Recipe = require("./recipe_model")
+
+        return {
+            recipes: {
+                relation: Model.ManyToManyRelation,
+                modelClass: Recipe,
+                join: {
+                    from: 'ingredients.id',
+                    through: {
+                        from: 'recipeingredient.ingredient_id',
+                        to: 'recipeingredient.recipe_id'
+                    },
+                    to: 'recipes.id'
+                }
+            }
+        }
+    }
 
 }
 
