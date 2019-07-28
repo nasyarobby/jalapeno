0ea46c9ff077c8ab4c8f526c5a57b9af2401125c
ElJalapenoVengador <eljalapenovengador@gmail.com>
2019-07-27 22:08:03 -0400

    addressed comments

--- routes/api/cookbooks/getRecentCookbooks.js
+++ routes/api/cookbooks/getRecentCookbooks.js
@@ -7,37 +7,40 @@ https://medium.com/@nicola.dallasen/express-knex-objection-painless-api-with-db-
 const Cookbook = require("./../../../models/cookbook_model");
 
 function getRecentCookbooks(req, res) {
-    let errors = [];
-    var num
     if (isNaN(req.params.num) || req.params.num < 0) {
-        errors.push({
-            message: "number of recent cookbooks is invalid"
-        })
-    }
-    else if (!req.params.num) {
-        num = 4;
+        let numOfCookbooks = [];
+        numOfCookbooks[0].message = "Number of Cookbooks is invalid or missing."
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSend.setFail({numOfCookbooks}).send());
+        return;
     }
     else {
-        num = req.params.num;
+        let num;
+        if (req.params.num == null) {
+            num = 4
+        }
+        else {
+            num = req.params.num;
+        }
+        Cookbook.query()
+            .eager("[owner, recipes]")
+            .orderBy([{ column: 'updated_at', order: 'desc' }, { column: 'id', order: 'desc' }])
+            .limit(num)
+            .then(cookbooks => {
+                for (row of cookbooks) {
+                    delete row.owner.password;
+                    row.name = row.cookbook_name;
+                    row.createdAt = row.created_at;
+                    row.updatedAt = row.updated_at;
+                    row.numOfRecipes = row.recipes.length;
+                    delete row.cookbook_name;
+                    delete row.created_at;
+                    delete row.updated_at;
+                }
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSend.setSuccess({cookbooks}).send());
+            })
     }
-    Cookbook.query()
-        .eager("[owner, recipes]")
-        .orderBy([{ column: 'updated_at', order: 'desc' }, { column: 'id', order: 'desc' }])
-        .limit(num)
-        .then(cookbooks => {
-            for (row of cookbooks) {
-                delete row.owner.password;
-                row.name = row.cookbook_name;
-                row.createdAt = row.created_at;
-                row.updatedAt = row.updated_at;
-                row.numOfRecipes = row.recipes.length;
-                delete row.cookbook_name;
-                delete row.created_at;
-                delete row.updated_at;
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSend.setSuccess({cookbooks}).send());
-        })
 }
 
 module.exports = getRecentCookbooks
