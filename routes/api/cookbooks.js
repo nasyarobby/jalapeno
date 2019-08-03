15a7b64837f6be2c1b1d3ffdccce31a905562789
ElJalapenoVengador <eljalapenovengador@gmail.com>
2019-08-02 22:04:34 -0400

    Pending cookbook puts

--- routes/api/cookbooks.js
+++ routes/api/cookbooks.js
@@ -8,12 +8,16 @@ var getCookbooksByUserId = require("./cookbooks/getCookbooksByUserId")
 var getRecipesByCookbookId = require("./cookbooks/getRecipesByCookbookId")
 var getDefaultRecentCookbooks = require("./cookbooks/getDefaultRecentCookbooks")
 
+var putCookbooks = require("./cookbooks/createCookbooks")
+
 
 router.get("/", getCookbooks)
 router.get("/recent/", getDefaultRecentCookbooks)
 router.get("/recent/:num", getRecentCookbooks)
 router.get("/user/:uid", getCookbooksByUserId)
 router.get("/id/:cid", getRecipesByCookbookId)
+
+router.put("/", putCookbooks)
 
 module.exports = {
     router: router
--- routes/api/cookbooks/getCookbooksByUserId.js
+++ routes/api/cookbooks/getCookbooksByUserId.js
@@ -5,36 +5,72 @@ https://medium.com/@nicola.dallasen/express-knex-objection-painless-api-with-db-
 */
 
 const Cookbook = require("../../../models/cookbook_model");
-const User = require('../../../models/user_model.js')
+const User = require('../../../models/user_model')
 
 function getCookbooksByUserId(req, res) {
-    let cookbooks;
-    let owner;
-    Cookbook.query()
-        .eager("[owner, recipes]")
-        .where("user_id", req.params.uid)
-        .orderBy([{ column: 'updated_at', order: 'desc' }, { column: 'id', order: 'desc' }])
-        .then(cbs => {
-            for (row of cbs) {
-                delete row.owner.password;
-                row.name = row.cookbook_name;
-                row.createdAt = row.created_at;
-                row.updatedAt = row.updated_at;
-                row.numOfRecipes = row.recipes.length;
-                delete row.cookbook_name;
-                delete row.created_at;
-                delete row.updated_at;
-            }
-            cookbooks = cbs
-            return User.query()
-                .where("id", req.params.uid)
+    User.query()
+        .where('username', req.params.uid)
+        .then(users => {
+            let cookbooks;
+            let owner;
+            let user = users[0];
+            Cookbook.query()
+                .eager("[owner, recipes]")
+                .where("user_id", user.id)
+                .orderBy([{ column: 'updated_at', order: 'desc' }, { column: 'id', order: 'desc' }])
+                .then(cbs => {
+                    for (row of cbs) {
+                        delete row.owner.password;
+                        row.name = row.cookbook_name;
+                        row.createdAt = row.created_at;
+                        row.updatedAt = row.updated_at;
+                        row.numOfRecipes = row.recipes.length;
+                        delete row.cookbook_name;
+                        delete row.created_at;
+                        delete row.updated_at;
+                    }
+                    cookbooks = cbs
+                    owner = user
+                    delete owner.password;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSend.setSuccess({cookbooks, owner}).send());
+                })
         })
-    .then(users => {
-        owner = users[0]
-        delete owner.password;
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSend.setSuccess({cookbooks, owner}).send());
-    })
+    
 }
+
+
+// function getCookbooksByUserId(req, res) {
+//     let cookbooks;
+//     let owner;
+//     Cookbook.query()
+//         .eager("[owner, recipes]")
+//         .where("user_id", req.params.uid)
+//         .orderBy([{ column: 'updated_at', order: 'desc' }, { column: 'id', order: 'desc' }])
+//         .then(cbs => {
+//             for (row of cbs) {
+//                 delete row.owner.password;
+//                 row.name = row.cookbook_name;
+//                 row.createdAt = row.created_at;
+//                 row.updatedAt = row.updated_at;
+//                 row.numOfRecipes = row.recipes.length;
+//                 delete row.cookbook_name;
+//                 delete row.created_at;
+//                 delete row.updated_at;
+//             }
+//             cookbooks = cbs
+//             return User.query()
+//                 .where("id", req.params.uid)
+//         })
+//     .then(users => {
+//         owner = users[0]
+//         delete owner.password;
+//         res.setHeader('Content-Type', 'application/json');
+//         res.send(JSend.setSuccess({cookbooks, owner}).send());
+//     })
+// }
+
+
+
 
 module.exports = getCookbooksByUserId
