d13a9797ae9be5ea2b6ba6685d063daa33c105bc
ElJalapenoVengador <eljalapenovengador@gmail.com>
2019-08-02 22:28:30 -0400

    createCook passed test, all tests passing

--- routes/api/cookbooks/createCookbook.js
+++ routes/api/cookbooks/createCookbook.js
@@ -1,3 +1,32 @@
-module.exports = function (req, res) {
-    res.send("Hello");
-};
+const JSend = new(require("../../../libs/jsend"))();
+const Cookbook = require("../../../models/cookbook_model");
+const User = require('../../../models/user_model')
+
+function putCookbook(req, res) {
+    console.log(req.body)
+    console.log(req.user)
+    let cname = req.body.name;
+    let category = req.body.category;
+    let user_id = req.user.id
+    Cookbook.query()
+        .insertAndFetch({
+            user_id: user_id,
+            cookbook_name: cname,
+            category: category
+        })
+        .then(cb => {
+            console.log(cb)
+            let id = cb.id;
+            let name = cb.cookbook_name;
+            let category = cb.category;
+            let createdAt = cb.created_at;
+            let owner = {
+                name: req.user.name,
+                id: user_id
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSend.setSuccess({id, name, category, createdAt, owner}).send());
+        })
+}
+
+module.exports = putCookbook;
