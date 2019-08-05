9a57062cdc3b96ac7e13ced12f625dadb4eb1d38
Nasyarobby <nasyarobby@gmail.com>
2019-08-04 23:43:35 -0400

    updated code, inserting succesfully for all data. needs to fetch user's info for cookbook's owner.

--- routes/api/recipes/createRecipe.js
+++ routes/api/recipes/createRecipe.js
@@ -1,8 +1,6 @@
 const JSend = new(require("../../../libs/jsend"))();
 const Recipe = require('../../../models/recipe_model');
 const Ingredient = require('../../../models/ingredient_model');
-const Cookbook = require("../../../models/cookbook_model");
-const Category = require("../../../models/category_model");
 
 function putRecipe(req, res) {
     let rname = req.body.name;
@@ -21,110 +19,97 @@ function putRecipe(req, res) {
         row.ingredient_name = row.name;
         delete row.name
     }
+    let existingIngredients;
+
     Ingredient.query()
+        .whereIn('ingredient_name', ringredients.map(i => i.ingredient_name))
         .then(ingredients => {
-            var recipeIngredientInDB = false;
-            for (rfoo = 0 ; rfoo < ringredients.length ; rfoo ++) {
-                recipeIngredientInDB = false;
-                for(ifoo = 0 ; ifoo < ingredients.length ; ifoo++) {
-                    if (ingredients[ifoo].ingredient_name == ringredients[rfoo].ingredient_name) {
-                        recipeIngredientInDB = true;
+            existingIngredients = ingredients;
+
+            let newIngredients = ringredients.map(e => e.ingredient_name)
+                .filter(e => {
+                    return ingredients.map(e => e.ingredient_name).indexOf(e) < 0;
+                })
+            if (newIngredients.length > 0) {
+                return Ingredient.query().insertAndFetch(newIngredients.map(e => {
+                    return {
+                        ingredient_name: e,
+                        location: "",
+                        price: 1.00
                     }
-                }
-                if (recipeIngredientInDB == false) {
-                    Ingredient.query()
-                        .insert({
-                            ingredient_name: ringredients[rfoo].ingredient_name
-                        })
-                }
+                }))
+            } else {
+                return []
             }
         })
-    Recipe.query()
-        .insertGraphAndFetch({
-            recipe_name: rname,
-            description: rdesc,
-            directions: rdirections,
-            preparationTime: rpreptime.toString() + " " + rpreptimeunit,
-            cookTime: rcooktime.toString() + " " + rcooktimeunit,
-            portions: rportions,
-            notes: rnotes,
-            ingredients: ringredients,
-            //cookbooks: rcookbookIds
-        }, {noDelete: true})
-        .then(rec => {
-            console.log(rec)
-            Ingredient.query()
-                .then(ingre => {
-                    console.log(ingre)
+        .then(result => {
+            /*
+            result = new ingredients
+            lets merge it to our existingIngredients
+            dont use merge, use push instead since it is faster
+            https://dev.to/uilicious/javascript-array-push-is-945x-faster-than-array-concat-1oki
+            */
+
+            Array.prototype.push.apply(existingIngredients, result);
+            // now we have the ids
+            // merge it back to our ringredients
+            ringredients.map(e => {
+                for (let i = 0; i < existingIngredients.length; i++) {
+                    const element = existingIngredients[i];
+                    if (element.ingredient_name == e.ingredient_name) {
+                        e.id = element.id;
+                        break;
+                    }
+                }
+                return e;
+            })
+
+            return Recipe.query()
+                .insertGraphAndFetch({
+                    recipe_name: rname,
+                    description: rdesc,
+                    directions: rdirections,
+                    preparationTime: rpreptime.toString() + " " + rpreptimeunit,
+                    cookTime: rcooktime.toString() + " " + rcooktimeunit,
+                    portions: rportions,
+                    notes: rnotes,
+                    ingredients: ringredients.map(e => {
+                        delete e.ingredient_name;
+                        return e;
+                    }),
+                    cookbooks: rcookbookIds,
+                    categories: [{
+                        id: rcategoryIds[0]
+                    }]
+
+                }, {
+                    relate: true
                 })
         })
+        .then(result => {
+            console.log(result);
+
+            // altering properties
+            result.name = result.recipe_name;
+            delete result.recipe_name;
+            result.categories[0].name = result.categories[0].category;
+            delete result.categories[0].category;
+            result.cookbooks[0].name = result.cookbooks[0].cookbook_name;
+            delete result.categories[0].cookbook_name;
+            result.createdAt = result.created_at;
+            delete result.created_at;
+            result.updatedAt = result.updated_at;
+            delete result.updated_at;
+
+            result.ingredients = result.ingredients.map(e => {
+                e.name = e.ingredient_name;
+                delete e.ingredient_name;
+                return e;
+            })
+
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSend.setSuccess(result).send())
+        })
 }
-
-    // Cookbook.query()
-    //     .where('id', rcookbookId)
-    //     .then(cookbook => {
-    //         for (row of rcategoryIds) {
-    //             Category.query()
-    //                 .where('id', row)
-    //                 .then(cat => {
-    //                     rcategories.push({
-    //                         id: cat[0].id,
-    //                         category: cat[0].category
-    //                     })
-    //                     //console.log(rcategories)
-    //                 })
-    //         }
-            // Recipe.query()
-            //     .upsertGraphAndFetch({
-            //         recipe_name: rname,
-            //         description: rdesc,
-            //         directions: rdirections,
-            //         preparationTime: rpreptime.toString() + " " + rpreptimeunit,
-            //         cookTime: rcooktime.toString() + " " + rcooktimeunit,
-            //         portions: rportions,
-            //         notes: rnotes,
-
-            //         cookbooks: cookbook,
-            //         categories: rcategories,
-            //         ingredients: ringredients
-            //     }, {relate: true})
-            //     .then(rec => {
-            //         console.log(req.body)
-            //         console.log(rec)
-            //         let id = rec.id;
-            //         let name = rec.recipe_name;
-            //         let description = rec.description;
-            //         let directions = rec.directions;
-            //         let preparationTime = rec.preparationTime;
-            //         let cookTime = rec.cookTime;
-            //         let portions = rec.portions;
-            //         let notes = rec.notes;
-            //         let categories = rec.categories;
-
-            //         res.setHeader('Content-Type', 'application/json');
-            //         res.send(JSend.setSuccess({id, name, description, directions, preparationTime, cookTime, portions, notes}).send());
-            //     })
-
-    // let cname = req.body.name;
-    // let category = req.body.category;
-    // let user_id = req.user.id
-    // Cookbook.query()
-    //     .insertAndFetch({
-    //         user_id: user_id,
-    //         cookbook_name: cname,
-    //         category: category
-    //     })
-    //     .then(cb => {
-    //         let id = cb.id;
-    //         let name = cb.cookbook_name;
-    //         let category = cb.category;
-    //         let createdAt = cb.created_at;
-    //         let owner = {
-    //             name: req.user.name,
-    //             id: user_id
-    //         }
-    //         res.setHeader('Content-Type', 'application/json');
-    //         res.send(JSend.setSuccess({id, name, category, createdAt, owner}).send());
-    //     })
 
 module.exports = putRecipe;
