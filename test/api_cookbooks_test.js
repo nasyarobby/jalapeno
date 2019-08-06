4e638710b54809d9bea13e680fa37a64d04a7202
Nasyarobby <nasyarobby@gmail.com>
2019-08-05 20:29:02 -0400

    added UPDATE apis

--- test/api_cookbooks_test.js
+++ test/api_cookbooks_test.js
@@ -502,7 +502,93 @@ describe("Cookbook API Routes", function () {
     })
 
     context("PUT /api/recipes/:rid", function () {
-        it("updates the recipe.")
+        it("updates the recipe.", function (done) {
+            let data = {
+                name: "Cake ABC",
+                description: "Some long description.",
+                directions: "some direction",
+                preparationTime: 10,
+                preparationTimeUnit: "hour",
+                cookTime: 0,
+                cookTimeUnit: "minute",
+                portions: 4,
+                notes: "Some notes",
+                ingredients: [{
+                        name: "Unknown Ing",
+                        quantity: 0.25,
+                        quantity_text: "1/4",
+                        unit: "cups"
+                    },
+                    {
+                        name: "Salt",
+                        quantity: 1,
+                        quantity_text: "",
+                        unit: "to taste"
+                    },
+                ],
+                cookbookId: [{
+                    id: 1
+                }],
+                categories: [2]
+            }
+
+            agent
+                .put("/api/recipes/1")
+                .set({
+                    "Authorization": "Bearer " + token
+                })
+                .send(data)
+                .end((err, res) => {
+                    if (err)
+                        done(err);
+
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.data.should.have.property("id");
+                    res.body.data.id.should.equal(1);
+                    res.body.data.should.have.property("name");
+                    res.body.data.name.should.equals(data.name);
+                    res.body.data.should.have.property("description");
+                    res.body.data.description.should.equals(data.description);
+                    res.body.data.should.have.property("directions");
+                    res.body.data.directions.should.equals(data.directions);
+                    res.body.data.should.have.property("preparationTime");
+                    res.body.data.preparationTime.should.equal("10 hour");
+                    res.body.data.should.have.property("cookTime");
+                    res.body.data.cookTime.should.equal("0 minute");
+                    res.body.data.should.have.property("portions");
+                    res.body.data.portions.should.equal(4);
+                    res.body.data.should.have.property("notes");
+                    res.body.data.notes.should.equal(data.notes);
+                    res.body.data.categories.should.be.an('array');
+                    res.body.data.categories.length.should.equal(1);
+                    res.body.data.categories[0].name.should.equal("Drink");
+
+                    res.body.data.ingredients.should.be.an('array');
+                    res.body.data.ingredients.length.should.equal(2);
+                    res.body.data.ingredients[0].should.have.property("quantity");
+                    res.body.data.ingredients[0].id.should.equal(76);
+                    res.body.data.ingredients[0].should.have.property("quantity_text");
+                    res.body.data.ingredients[0].should.have.property("unit");
+                    // name is ingredient_name in the database
+                    res.body.data.ingredients[0].should.have.property("name");
+                    res.body.data.ingredients[0].name.should.equal(data.ingredients[0].name);
+
+                    res.body.data.ingredients[1].id.should.satisfy(id => id == 76 || id == 6)
+
+                    res.body.data.should.have.property("createdAt");
+
+                    //cookbooks should be an array because its many to many relation
+                    res.body.data.cookbooks.should.be.an("array");
+                    res.body.data.cookbooks[0].name.should.equal("Watching My Figure");
+
+                    res.body.data.cookbooks[0].owner.id.should.equal(1);
+                    res.body.data.cookbooks[0].owner.name.should.equal("Alice Peace");
+                    res.body.data.cookbooks[0].owner.should.not.have.property("password");
+
+                    done();
+                })
+        })
     })
 
     context("DELETE /api/cookbooks/id/:cid", function () {
